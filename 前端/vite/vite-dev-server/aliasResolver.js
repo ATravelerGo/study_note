0d7ebe8e9c14dea1acb90f428b2ecf2f76567c0c
chen <xiaokeai1833103@163.com>
2024-09-09 20:04:14 +0800

    zhang

--- 前端/vite/vite-dev-server/aliasResolver.js
+++ 前端/vite/vite-dev-server/aliasResolver.js
@@ -1,20 +1,16 @@
-module.exports=function (aliasConf,jsContent){
+module.exports = function (aliasConf, jsContent) {
+  const entries = Object.entries(aliasConf);
+  let lastContent = jsContent;
+  console.log("jsContent", jsContent);
+  entries.forEach((entry) => {
+    const [alia, path] = entry;
+    console.log("path", path);
+    const srcIndex = path.indexOf("/src");
+    const realPath = path.slice(srcIndex, path.length);
+    console.log("realPath", realPath);
+    lastContent = jsContent.replace(alia, realPath);
+    console.log("lastContent", lastContent);
+  });
 
-    const entries=Object.entries(aliasConf)
-    let lastContent=jsContent
-    console.log("jsContent",jsContent)
-    entries.forEach((entry)=>{
-        const [alia,path] =entry
-        console.log("path",path)
-        const srcIndex=path.indexOf('/src') //这里有问题 需要解决
-        const realPath=path.slice(srcIndex,path.length)
-        console.log("realPath",realPath)
-        lastContent=jsContent.replace(alia,realPath)
-        console.log("lastContent",lastContent)
-    })
-
-
-    return jsContent
-
-
-}
+  return lastContent;
+};
