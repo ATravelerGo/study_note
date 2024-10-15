ed2b0d8b6c8f23fc24c54d733f61d2dfd517550c
ATravelerGo <xiaokeai1833103@163.com>
2024-10-15 18:55:35 +0800

    zhang

--- Back_End/CPlus/CPlusNote/CPlusNote/06_Virtual.cpp
+++ Back_End/CPlus/CPlusNote/CPlusNote/06_Virtual.cpp
@@ -1,47 +1,50 @@
 #include<iostream>
 
+namespace Virtual {
+	class Entity
+	{
+	public:
 
-class Entity
-{
-public:
-
-	virtual std::string GetName() { return "Entity"; }
-
-	
-
-private:
-
-};
-
-class Player:public Entity
-{
-public:
-	Player(const std::string& name) {
-		m_name = name;
-	}
-	
-	std::string GetName() override {
-		return m_name;
-	}
-
-private:
-	std::string m_name;
-};
+		virtual std::string GetName() { return "Entity"; }
 
 
 
+	private:
+
+	};
+
+	class Player :public Entity
+	{
+	public:
+		Player(const std::string& name) {
+			m_name = name;
+		}
+
+		std::string GetName() override {
+			return m_name;
+		}
+
+	private:
+		std::string m_name;
+	};
+
+}
 
 
 
-void main() {
 
-	Entity* e = new Entity();
+
+
+
+void main6() {
+
+	Virtual::Entity* e = new Virtual::Entity();
 	std::cout << e->GetName() << std::endl;
 
-	Player* p = new Player("张辰");
+	Virtual::Player* p = new Virtual::Player("张辰");
 	std::cout << p->GetName() << std::endl;
 
-	Entity* e1 = p;
+	Virtual::Entity* e1 = p;
 	std::cout << e1->GetName() << std::endl; //打印出来是entity
 
 	std::cout << "hello" << std::endl;
---
+++ Back_End/CPlus/CPlusNote/CPlusNote/07_PureVirtual.cpp
@@ -0,0 +1,27 @@
+
+
+#include<iostream>
+namespace PureVirtual {
+
+	class Entity
+	{
+	public:
+		virtual std::string _Gettnames() = 0;
+	private:
+
+	};
+
+	
+
+
+
+
+
+}
+
+
+
+
+void main() {
+
+}
