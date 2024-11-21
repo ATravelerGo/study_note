5156fde7b1d3c02a2fdaa23bb715a85566d5e050
chen <xiaokeai1833103@gmail.com>
2024-11-21 09:55:58 +0800

    zhang

---
+++ Front_End/transformAndTransformGroup/animationStudy/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
