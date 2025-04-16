e7f56afdfc57816e8b279d054b44a09d6be341a9
chen <xiaokeai1833103@163.com>
2025-04-16 17:39:00 +0800

    zhang

--- Front_End/项目模版vue3+ts/mini_patients_information/vite.config.ts
+++ Front_End/项目模版vue3+ts/mini_patients_information/vite.config.ts
@@ -20,6 +20,42 @@ export default defineConfig({
 			'@': path.resolve(__dirname, 'src')
 		}
 	},
+	build: {
+		outDir: 'dist_uni_patients_information',         // 打包目录
+		assetsDir: 'static',    // 静态资源目录（js, css, img）
+		sourcemap: false,       // 生产环境关闭 source map
+		minify: 'esbuild',       // 可选 esbuild（快）或 terser（可配）
+		terserOptions: {
+			compress: {
+				drop_console: true, // 移除 console
+				drop_debugger: true
+			}
+		},
+
+		rollupOptions: {
+
+			output: {
+				// JS：入口文件和动态导入的 chunk
+				entryFileNames: 'static/js/[name]-[hash].js',
+				chunkFileNames: 'static/js/[name]-[hash].js',
+
+				// 静态资源文件：图片、字体、CSS 等
+				assetFileNames: (assetInfo) => {
+					const ext = assetInfo.name?.split('.').pop();
+
+					// 根据文件类型分目录
+					if (/css/i.test(ext!)) return 'static/css/[name]-[hash][extname]';
+					if (/(png|jpe?g|gif|svg|webp|ico)/i.test(ext!)) return 'static/img/[name]-[hash][extname]';
+					if (/(ttf|woff2?|eot)/i.test(ext!)) return 'static/fonts/[name]-[hash][extname]';
+
+					// 其他默认放 assets
+					return 'static/assets/[name]-[hash][extname]';
+				}
+			}
+		},
+
+		chunkSizeWarningLimit: 1000 // 单文件超过多少 KB 警告
+	},
 	//服务器配置
 	server: {
 		//配置端口
