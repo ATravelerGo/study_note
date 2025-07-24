93217d50696d480b558fb6f5898133c340f1fa83
Chen <xiaokeai1833103@163.com>
2025-07-24 15:02:23 +0800

    zhang

--- Electron/main.js
+++ Electron/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
 const path = require("node:path");
+let bluetoothPinCallback;
+let selectBluetoothCallback;
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -8,9 +10,29 @@ const createWindow = () => {
       preload: path.join(__dirname, "./src/preload.js"),
     },
   });
-
+  // 选择蓝牙配对
+  win.webContents.on("select-bluetooth-device", (event, devices, callback) => {
+    event.preventDefault();
+    console.log("devices", devices);
+    selectBluetoothCallback = callback;
+  });
+  win.webContents.session.setBluetoothPairingHandler((details, callback) => {
+    bluetoothPinCallback = callback;
+    // Send a message to the renderer to prompt the user to confirm the pairing.
+    win.webContents.send("bluetooth-pairing-request", details);
+  });
   win.loadFile("index.html");
 };
+
+//取消蓝牙配对
+ipcMain.on("cancel-bluetooth-request", () => {
+  selectBluetoothCallback("");
+});
+
+// 渲染进程告诉主进程用户的蓝牙配对结果，主进程再把结果反馈给 Electron 蓝牙系统
+ipcMain.on("bluetooth-pairing-response", (event, response) => {
+  bluetoothPinCallback(response);
+});
 
 // 修改主题色
 ipcMain.handle("dark-mode:toggle", () => {
