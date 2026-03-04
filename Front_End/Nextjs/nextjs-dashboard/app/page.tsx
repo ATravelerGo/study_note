0cd3f01b47f5b482decbc0b37fb8a53e185505da
chen <xiaokeai1833103@163.com>
2026-03-04 19:53:13 +0800

    feat(nextjs-dashboard): add initial configuration and layout for Next.js application
    - Create .gitignore files to exclude unnecessary files and directories.
    - Add module configuration files for project structure in .idea directory.
    - Implement global styles and font imports in layout component.
    - Enhance page component with new font and image handling.
    - Introduce a new dashboard page with a simple "Hello World" message.
    - Add font utility for managing Google Fonts in the application.

--- Front_End/Nextjs/nextjs-dashboard/app/page.tsx
+++ Front_End/Nextjs/nextjs-dashboard/app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -10,7 +12,8 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"></div>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} `}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -26,6 +29,8 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image src="/hero-desktop.png" alt="Hero" width={1000} height={760} className='hidden md:block' />
+          <Image src="/hero-mobile.png" alt="Hero" width={560} height={620} className='block md:hidden' />
         </div>
       </div>
     </main>
